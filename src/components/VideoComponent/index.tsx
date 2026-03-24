f9f3a3ef3ffa20c40bb541609314b15d287470af
renat <renat0014@gmail.com>
2026-03-24 13:41:56 +0400

    fix: pause ad gpt urls

--- src/components/VideoComponent/index.tsx
+++ src/components/VideoComponent/index.tsx
@@ -2,8 +2,31 @@ import Hls from "hls.js";
 import { Preload, Video, VideoIFrame, InteractNote } from './styles'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import { VideoPlayerCallback, VideoPlayerData, useStreamLayer } from "@streamlayer/react"
-import { StreamLayerSDKAdvertisement } from "@streamlayer/react/advertisement"
 import { StreamLayerPauseAd } from "@streamlayer/react/pause-ad"
+
+const pauseAdPresets = {
+  streamlayer_slot: [{
+    template: 'default' as const,
+    url: '/23213969138/pause_ad_ctv',
+    adWidth: 315,
+    adHeight: 400,
+  }],
+  host_url: [{
+    template: 'default' as const,
+    url: 'https://pagead2.googlesyndication.com/gampad/ads?pvsid=7320384242204022&correlator=649374465758607&output=ldjh&gdfp_req=1&vrg=202602170101&ptt=17&impl=fif&iu_parts=5479%2Cctv.tsn.ca%2Cnba&enc_prev_ius=%2F0%2F1%2F2&prev_iu_szs=315x400&ifi=1&dids=jasper-player-pause-ad-overlay&adfs=2601896588&sfv=1-0-45&eri=1&sc=1&abxe=1&dt=1772017069904&lmt=1741681952&adxs=-12245933&adys=-12245933&biw=1920&bih=1080&scr_x=0&scr_y=0&btvi=-1&ucis=1&oid=2&u_his=4&u_h=1080&u_w=1920&u_ah=1080&u_aw=1920&u_cd=24&u_sd=1&u_tz=60&dmc=8&bc=31&nvt=1&uach=WyJtYWNPUyIsIjI2LjIuMCIsImFybSIsIiIsIjE0NS4wLjc2MzIuMTE2IixudWxsLDAsbnVsbCwiNjQiLFtbIk5vdDpBLUJyYW5kIiwiOTkuMC4wLjAiXSxbIkdvb2dsZSBDaHJvbWUiLCIxNDUuMC43NjMyLjExNiJdLFsiQ2hyb21pdW0iLCIxNDUuMC43NjMyLjExNiJdXSwwXQ..&uas=3&url=https%3A%2F%2Fbl-tsn-lg.bl-massive.com%2F%23%2Fwatch%2FNBA_Thunder_116_Raptors_107_304596%3F_k%3Drijhbk&vis=1&psz=0x-1&msz=0x-1&fws=644&ohw=1920&topics=5&tps=5&htps=5&psd=WzMxLFtdXQ..&dlt=1772016931070&idt=823&adks=3544135819&frm=20&eo_id_str=ID%3De87a1033aa910566%3AT%3D1765209356%3ART%3D1771259724%3AS%3DAA-AfjbWMgNC70JctstXr4VITd6l&pgls=CAk.',
+    product: 'tsn',
+    platform: 'cotv',
+    platformtype: 'amazonfire',
+    pagetype: 'planepage',
+    content: 'cfl-news-and-highlights',
+  }],
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+} satisfies Record<string, [{ template: 'default'; url: string; [key: string]: unknown }]>
+
+const searchParams = new URLSearchParams(window.location.search)
+
+// @ts-ignore
+const pauseAdPreset = pauseAdPresets[searchParams.get('pause_ad')] || null
 
 export const VideoComponent: React.FC<{ muted: boolean, setMuted: React.Dispatch<React.SetStateAction<boolean>>, interacted: boolean, setInteracted: (interacted: boolean) => void
 }> = ({ interacted, setInteracted, muted }) => {
@@ -111,32 +134,26 @@ export const VideoComponent: React.FC<{ muted: boolean, setMuted: React.Dispatch
         onRenderPauseAd={onRenderPauseAd}
         onClosePauseAd={onClosePauseAd}
         videoPlayerController={videoPlayerController}
-        pauseAdVastUrl={pauseAdUrl ?[
+        pauseAdExternalUrls={pauseAdPreset || (pauseAdUrl ? [
           {
             template: 'default',
             url: pauseAdUrl,
           },
-        ] : undefined}
+        ] : undefined)}
       >
-        <StreamLayerSDKAdvertisement
-          muted={!muted}
-          persistent
-          sideBySide
-        >
-          <Video
-            src={streamSrc}
-            ref={videoRef}
-            muted={muted}
-            onPlay={onVideoPlayWithState}
-            onPause={onVideoPauseWithState}
-            onEnded={onVideoPauseWithState}
-            autoPlay
-            loop
-            playsInline
-            controls={showControls}
-            controlsList="nodownload nofullscreen noremoteplayback"
-          />
-        </StreamLayerSDKAdvertisement>
+        <Video
+          src={streamSrc}
+          ref={videoRef}
+          muted={muted}
+          onPlay={onVideoPlayWithState}
+          onPause={onVideoPauseWithState}
+          onEnded={onVideoPauseWithState}
+          autoPlay
+          loop
+          playsInline
+          controls={showControls}
+          controlsList="nodownload nofullscreen noremoteplayback"
+        />
       </StreamLayerPauseAd>
       {/* {showControls && <VideoMuteButton onClick={() => setMuted((prev) => !prev)}>{muted ? 'unmute' : 'mute'}</VideoMuteButton>} */}
       {!interacted && errOnPlay && <InteractNote>Click to start</InteractNote>}
