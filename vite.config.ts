1216a4b985f56d225c69e754161c114897aa34ce
renat <renat0014@gmail.com>
2026-04-16 17:33:52 +0500

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [react(), EnvironmentPlugin("all")],
+  plugins: [
+    react(),
+    EnvironmentPlugin('all', { prefix: 'VITE_' }),
+  ],
 })
